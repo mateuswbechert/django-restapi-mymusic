aa17ade22b240c4ee51aba90e99daa92dd6412b3
Mateus WB <mateus.wolfart@f1solucoes.com.br>
2022-01-12 17:52:55 -0300

    Registered models on django-admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.apps import apps
+from .models import Music, Album, Band, Member
 
-# Register your models here.
+admin.site.register(Band)
+admin.site.register(Album)
+admin.site.register(Music)
+admin.site.register(Member)
