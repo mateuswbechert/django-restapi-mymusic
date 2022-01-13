c52fb70d5aee729f9bc64f5a57cc6634c8d9856e
Mateus WB <mateuswbechert@gmail.com>
2022-01-12 21:25:09 -0300

    Members of the band may be inserted on new band page

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from django.apps import apps
+from django.db.models import fields
 from .models import Music, Album, Band, Member
 
-admin.site.register(Band)
+class MemberInline(admin.TabularInline):
+    model = Member
+    extra = 3
+
+class BandAdmin(admin.ModelAdmin):
+    inlines = [MemberInline]
+
+admin.site.register(Band, BandAdmin)
 admin.site.register(Album)
 admin.site.register(Music)
 admin.site.register(Member)
